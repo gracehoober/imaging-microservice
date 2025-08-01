572735e909764b0c8b247715e0565de57b57c8ee
Grace Bethany Hoober <grace.hoober66@gmail.com>
2025-08-01 13:31:17 -0700

    feat: :sparkles: start models

---
+++ README.md
@@ -0,0 +1,22 @@
+# Imaging MicroService
+
+Imaging Microservice is a small API focusing on delievering medical imaging to the client.
+
+## Features
+- Uploading imaging to blobs.
+- REST endpoints for medical imaging including filtering and pagination.
+
+## Tech Stack
+- Django
+- DjangoRestFramework
+- Blobs
+- PostgreSQL
+- Python
+
+## Installation
+```
+git clone https://github.com/gracehoober/imaging-microservice
+cd imaging-microservice
+pip install requirements.txt
+set up your local postgres db
+```
---
+++ imaging_service/imaging/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Images(models.Model):
+    MODALITY_CHOICES = (
+        ("CT", "Computed Tomography"),
+        ("CTA", "Computed Tomography Angiography"),
+        ("XR", "X-ray"),
+        ("MRI", "Magnetic Resonance Imaging"),
+    )
+
+    id = models.UUIDField(null=False)
+    url = models.URLField(null=False)
+    created_at = models.DateTimeField(null=False)
+    modality = models.CharField(max_length=50, choices=MODALITY_CHOICES, blank=False)
+    patient_id = models.UUIDField(null=False)
